705a711449c2d70a67304f2c1dcf77b2ef8ce951
bayuaji17 <nugroho.bayuaji17@gmail.com>
2024-12-18 20:41:06 +0700

    test CI

--- src/route/public-api.ts
+++ src/route/public-api.ts
@@ -3,3 +3,5 @@ import { UserController } from "../controller/user-controller";
 
 export const publicRouter = express.Router();
 publicRouter.post("/api/users", UserController.register);
+// Test trigger CI
+publicRouter.get("/api/users", UserController.register);
