b3a79e7d254d1ec654bb9536c9cb6715ace35c42
bayuaji17 <nugroho.bayuaji17@gmail.com>
2024-12-29 00:24:45 +0700

    test ci cd

--- src/route/public-api.ts
+++ src/route/public-api.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { UserController } from "../controller/user-controller";
+// TEST CI CD Pipeline
 
 export const publicRouter = express.Router();
 publicRouter.post("/api/users", UserController.register);
+publicRouter.post("/api/test", UserController.register);
